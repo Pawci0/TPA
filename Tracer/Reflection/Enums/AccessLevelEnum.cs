f5cc3f752f51385c978372530682999b64b7f095
P G <pgalewicz@gmail.com>
2018-11-03 20:23:13 +0100

    added Refletion project and enums inside it

---
+++ Tracer/Reflection/Enums/AccessLevelEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection.Enums
+{
+    public enum AccessLevelEnum
+    {
+        Public,
+        Protected,
+        ProtectedInternal,
+        Private
+    }
+}
