c5a5244e17cd42d520f8c7779e701d26298e67ac
Szymon <szymonicho@gmail.com>
2018-11-04 10:30:10 +0100

    added Metadata files and extension methods

--- Tracer/Reflection/Enums/AccessLevelEnum.cs
+++ Tracer/Reflection/Enums/AccessLevelEnum.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Reflection.Enums
+﻿namespace Reflection.Enums
 {
     public enum AccessLevelEnum
     {
-        Public,
-        Protected,
-        ProtectedInternal,
-        Private
+        IsPublic,
+        IsProtected,
+        IsProtectedInternal,
+        IsPrivate
     }
 }
---
+++ Tracer/Reflection/Metadata/ParameterMetadata.cs
@@ -0,0 +1,17 @@
+﻿namespace Reflection
+{
+    internal class ParameterMetadata
+    {
+
+        public ParameterMetadata(string name, TypeMetadata typeMetadata)
+        {
+            this.m_Name = name;
+            this.m_TypeMetadata = typeMetadata;
+        }
+
+        //private vars
+        private string m_Name;
+        private TypeMetadata m_TypeMetadata;
+
+    }
+}
