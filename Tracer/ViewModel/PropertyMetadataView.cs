51745bd491c222672d70bac0d8048872ccef1fbb
P G <pgalewicz@gmail.com>
2018-11-07 17:27:44 +0100

    csproj fix

---
+++ Tracer/ViewModel/PropertyMetadataView.cs
@@ -0,0 +1,30 @@
+﻿using Reflection.Metadata;
+using System;
+using System.Collections.ObjectModel;
+
+namespace ViewModel.MetadataViews
+{
+    class PropertyMetadataView : BaseMetadataView, IExpander
+    {
+        private PropertyMetadata propertyMetadata;
+
+        public PropertyMetadataView(PropertyMetadata _propertyMetadata) 
+            : base(_propertyMetadata.m_Name)
+        {
+            propertyMetadata = _propertyMetadata;
+        }
+
+        public void Expand(ObservableCollection<TreeViewItem> children)
+        {
+            if(propertyMetadata.m_TypeMetadata != null)
+            {
+                base.Add(propertyMetadata.m_TypeMetadata, children);
+            }
+        }
+
+        public override string ToString()
+        {
+            return propertyMetadata.m_TypeMetadata.m_typeName + " " + propertyMetadata.m_Name;
+        }
+    }
+}
