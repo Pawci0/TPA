4373e111a16bb89cf27aeb5af1850d3fd9958d76
Szymon <szymonicho@gmail.com>
2018-11-28 21:48:59 +0100

    added test for serialization

---
+++ Tracer/SerializationTests/SerializationTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Serialization.Tests
+{
+    [TestClass()]
+    public class SerializationTests
+    {
+        [TestMethod()]
+        public void SerializationTest()
+        {
+            string originalObject = "original string";
+            XMLSerializer serializer = new XMLSerializer();
+            string path = "SerializationTestFile.xml";
+            serializer.Serialize(path, originalObject);
+            string deserializedObject = serializer.Deserialize<string>(path);
+            Assert.AreEqual(originalObject, deserializedObject);
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+    }
+}
