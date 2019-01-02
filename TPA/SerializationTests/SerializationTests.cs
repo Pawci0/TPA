3028e0e3ad95d1e87a0dd4f304bcb6287f592aaa
P G <pgalewicz@gmail.com>
2019-01-02 21:50:20 +0100

    reworked serialization tests

--- TPA/SerializationTests/SerializationTests.cs
+++ TPA/SerializationTests/SerializationTests.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DTGBase;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Serialization.Tests
 {
@@ -9,13 +12,29 @@ namespace Serialization.Tests
         [TestMethod()]
         public void SerializationTest()
         {
-            string originalObject = "original string";
-            //XMLSerializer serializer = new XMLSerializer();
+            List<NamespaceBase> list = new List<NamespaceBase>()
+            {
+                new NamespaceBase()
+                {
+                    name = "n1"
+                },
+                new NamespaceBase()
+                {
+                    name = "n2"
+                }
+            };
+            AssemblyBase originalObject = new AssemblyBase()
+            {
+                name = "test",
+                namespaces = list
+            };
+            XMLSerializer serializer = new XMLSerializer();
             string path = "SerializationTestFile.xml";
-            //serializer.Serialize(path, originalObject);
-            //string deserializedObject = serializer.Deserialize<string>(path);
-            //Assert.AreEqual(originalObject, deserializedObject);
-            Assert.IsTrue(true);
+            serializer.Serialize(path, originalObject);
+            AssemblyBase deserializedObject = serializer.Deserialize(path);
+            Assert.AreEqual(originalObject.name, deserializedObject.name);
+            Assert.AreEqual(originalObject.namespaces.ToList()[0].name, deserializedObject.namespaces.ToList()[0].name);
+            Assert.AreEqual(originalObject.namespaces.ToList()[1].name, deserializedObject.namespaces.ToList()[1].name);
             if (File.Exists(path))
             {
                 File.Delete(path);
