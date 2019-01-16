df81e11b4b4b57224308b05b14737acbdc811c0e
Szymon <szymonicho@gmail.com>
2019-01-16 17:08:41 +0100

    database serializer cont.

--- TPA/Database/DBSerializer.cs
+++ TPA/Database/DBSerializer.cs
@@ -1,5 +1,9 @@
-﻿using DTGBase;
+﻿using Database.Model;
+using DTGBase;
 using Interfaces;
+using System;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Database
 {
@@ -7,12 +11,66 @@ namespace Database
     {
         public AssemblyBase Deserialize(string filePath)
         {
-            throw new System.NotImplementedException();
+            using(var ctx = new DatabaseContext())
+            {
+                ctx.Configuration.ProxyCreationEnabled = false;
+                ctx.NamespaceModel
+                    .Include(n => n.Types)
+                    .Load();
+                ctx.TypeModel
+                    .Include(t => t.Constructors)
+                    .Include(t => t.BaseType)
+                    .Include(t => t.DeclaringType)
+                    .Include(t => t.Fields)
+                    .Include(t => t.ImplementedInterfaces)
+                    .Include(t => t.GenericArguments)
+                    .Include(t => t.Methods)
+                    .Include(t => t.NestedTypes)
+                    .Include(t => t.Properties)
+                    .Include(t => t.TypeGenericArguments)
+                    .Include(t => t.TypeImplementedInterfaces)
+                    .Include(t => t.TypeNestedTypes)
+                    .Include(t => t.MethodGenericArguments)
+                    .Include(t => t.TypeBaseTypes)
+                    .Include(t => t.TypeDeclaringTypes)
+                    .Load();
+                ctx.ParameterModel
+                    .Include(p => p.Type)
+                    .Include(p => p.TypeFields)
+                    .Include(p => p.MethodParameters)
+                    .Load();
+                ctx.MethodModel
+                    .Include(m => m.GenericArguments)
+                    .Include(m => m.Parameters)
+                    .Include(m => m.ReturnType)
+                    .Include(m => m.TypeConstructors)
+                    .Include(m => m.TypeMethods)
+                    .Load();
+                ctx.PropertyModel
+                    .Include(p => p.Type)
+                    .Include(p => p.TypeProperties)
+                    .Load();
+
+                DatabaseAssembly dbAssembly = ctx.AssemblyModel
+                    .Include(a => a.Namespaces)
+                    .ToList().FirstOrDefault();
+
+                if(dbAssembly == null)
+                {
+                    throw new ArgumentException("Database is empty");
+                }
+                return DTGMapper.ToBase(dbAssembly);
+            }
         }
 
         public void Serialize(string filePath, AssemblyBase target)
         {
-            throw new System.NotImplementedException();
+            DatabaseAssembly serializationModel = new DatabaseAssembly(target);
+            using (var ctx = new DatabaseContext())
+            {
+                ctx.AssemblyModel.Add(serializationModel);
+                ctx.SaveChanges();
+            }
         }
     }
 }
--- TPA/TUI/ConsoleFileSupplier.cs
+++ TPA/TUI/ConsoleFileSupplier.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.ComponentModel.Composition;
 using System.IO;
-using ViewModel;
+using Interfaces;
 
 namespace TUI
 {
