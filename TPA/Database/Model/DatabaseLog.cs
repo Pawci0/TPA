b39c4444674f18ce623ea1f81aca2cb4aa36919f
P G <pgalewicz@gmail.com>
2019-01-16 21:07:12 +0100

    added id to DatabaseLog entity

--- TPA/Database/Model/DatabaseLog.cs
+++ TPA/Database/Model/DatabaseLog.cs
@@ -4,6 +4,7 @@ namespace Database.Model
 {
     public class DatabaseLog
     {
+        public int Id { get; set; }
         public string Message { get; set; }
         public DateTime Timestamp { get; set; }
         public string TraceLevel { get; set; }
