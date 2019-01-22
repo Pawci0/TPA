821d0a63ab20a9eca9f90ad1c37f89b0db86989a
Szymon <szymonicho@gmail.com>
2019-01-22 17:06:29 +0100

    siema

---
+++ TPA/DatabaseTests1/DBSerializerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Tests
+{
+    [TestClass()]
+    public class DBSerializerTests
+    {
+        [TestMethod()]
+        public void DeserializeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SerializeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
